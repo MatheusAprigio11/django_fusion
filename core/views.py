f9a2508b62f84d95b6bcd0a25fd8525130945f59
Matheus Aprigio <matheusaprigio11>
2023-11-07 22:36:49 -0300

    updt

--- core/views.py
+++ core/views.py
@@ -1,8 +1,16 @@
+from typing import Any
 from django.views.generic import TemplateView
-
+from .models import Servico, Funcionario, Feature
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self). get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        context['features'] = Feature.objects.all()
+        return context
 
 
 
